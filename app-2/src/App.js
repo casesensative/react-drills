401175e45abe28ae3ced265cfc894fa1faad1d97
casesensative <victor.waters1@gmail.com>
2021-04-27 12:43:23 -0400

    App-2 complete

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      list: ['this', 'is', 'an', 'array', 'of', 'strings'],
+    }
+  }
+
+  render() {
+      let list = this.state.list.map((e) => {
+        return <h2 className="App">{e}</h2>
+      })
+
+    return (
+      <section className="main">
+        {list}
+      </section>
+    )
+  }
 }
 
 export default App;
