3d448125e15db6302c14c693ef24f34096801974
casesensative <victor.waters1@gmail.com>
2021-04-27 11:54:47 -0400

    App-1 Complete

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  inputHandler = (inputText) => {
+    this.setState({input: inputText})
+  }
+
+  render() {
+    return (
+      <section>
+        <h1 className="App">TEXT DISPLAY:</h1>
+        <input className="App" type="text" placeholder="enter text here" onChange={e => this.inputHandler(e.target.value)}></input>
+        {this.state.input}
+      </section>
+    )
+  }
 }
 
 export default App;
